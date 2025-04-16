a1426bef424cdb36fb5ab58f258cd33ac55af2f9
BMPMS <milesbryony@googlemail.com>
2025-04-16 10:11:19 +0100

    fix: latest changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const repoName = "italianPrisons"; // 🔁 change this to your repo name
-
-const isGitHubPages = process.env.DEPLOY_ENV === "GH_PAGES";
-
 const nextConfig: NextConfig = {
-    output: "export", // ⬅️ this tells Next.js to use `next export`
-    basePath: isGitHubPages ? `/${repoName}` : "",
-    assetPrefix: isGitHubPages ? `/${repoName}/` : "",
-    trailingSlash: true,
+    /* config options here */
+    output: 'export',
 };
+
 export default nextConfig;

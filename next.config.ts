617839e6ce5c99381b144f4fb62cd45c7c33c4b5
BMPMS <milesbryony@googlemail.com>
2025-04-16 10:55:45 +0100

    fix: main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-    /* config options here */
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Required for static export on GitHub Pages
+    },
+    assetPrefix: isProd ? '/italianPrisons/' : '',
+    basePath: isProd ? '/italianPrisons' : '',
     output: 'export',
 };
 
